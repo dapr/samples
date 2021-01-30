f9a231a440eb2c5ef50086493c4a00274b0e2b5e
Donovan Brown <dbrown@microsoft.com>
2021-01-29 23:30:02 -0600

    removing the vendor folder

--- twitter-sentiment-processor/demos/demo2/viewer/vendor/github.com/cloudevents/sdk-go/v2/binding/spec/doc.go
+++
@@ -1,9 +0,0 @@
-package spec
-
-/*
-Package spec provides spec-version metadata.
-
-For use by code that maps events using (prefixed) attribute name strings.
-Supports handling multiple spec versions uniformly.
-
-*/

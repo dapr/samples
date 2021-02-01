4d6192c5264ee04f839bb7f9dd407a885f8a6320
Donovan Brown <dbrown@microsoft.com>
2021-02-01 15:15:59 -0600

    Adding location for those that don't have defaults set. Found this testing on virgin machine.

--- twitter-sentiment-processor/demos/demo2/setup.ps1
+++ twitter-sentiment-processor/demos/demo2/setup.ps1
@@ -19,11 +19,18 @@ param (
       HelpMessage = "The version of the dapr runtime version to deploy."
    )]
    [string]
-   $daprVersion = "1.0.0-rc.3"
+   $daprVersion = "1.0.0-rc.3",
+
+   [Parameter(
+      Position = 2,
+      HelpMessage = "The location to store the meta data for the deployment."
+   )]
+   [string]
+   $location = "eastus"
 )
 
 # Deploy the infrastructure
-$deployment = $(az deployment sub create --template-file .\main.json --parameters rgName=$rgName --output json) | ConvertFrom-Json
+$deployment = $(az deployment sub create --location $location --template-file .\main.json --parameters rgName=$rgName --output json) | ConvertFrom-Json
 
 # Get all the outputs
 $cognitiveServiceKey = $deployment.properties.outputs.cognitiveServiceKey.value
